dcb0d64526d73c82725a6741e240f90e135b26d3
K. Scott Allen <scott@OdeToCode.com>
2014-12-12 17:38:26 -0500

    adding ui features

---
+++ ngPlaybook/Apps/ui/module.js
@@ -0,0 +1,11 @@
+﻿(function () {
+
+    var module = angular.module("ui", ["common", "ui.router"]);
+
+    module.config(function ($stateProvider, $urlRouterProvider) {
+
+        $urlRouterProvider.otherwise("/dashboard");
+        $stateProvider.state("deashboard", { url: "/dashboard", templateUrl: "templates/dashboard.html" });
+    });
+
+}());
