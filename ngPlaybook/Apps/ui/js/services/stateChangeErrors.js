d18330b488d2d5030fd1fd74208dfa2f29da5cf9
K. Scott Allen <scott@OdeToCode.com>
2014-12-19 16:02:21 -0500

    adding nested ui-router views

---
+++ ngPlaybook/Apps/ui/js/services/stateChangeErrors.js
@@ -0,0 +1,9 @@
+﻿(function (module) {
+
+    module.run(function($rootScope, alerting){
+        $rootScope.$on("$stateChangeError", function (event, toState, toParams, fromState, fromParams, error) {
+            alerting.addDanger("Could not load " + toState.name);
+        });
+    });
+
+}(angular.module("ui")));
--- ngPlaybook/Apps/ui/module.js
+++ ngPlaybook/Apps/ui/module.js
@@ -4,8 +4,13 @@
 
     module.config(function ($stateProvider, $urlRouterProvider) {
 
-        $urlRouterProvider.otherwise("/dashboard");
-        $stateProvider.state("deashboard", { url: "/dashboard", templateUrl: "templates/dashboard.html" });
+        $urlRouterProvider.otherwise("/dashboard/overall");
+        $stateProvider
+            .state("dashboard", { url: "/dashboard", templateUrl: "templates/dashboard.html" })
+                .state("overll", { parent:"dashboard", url: "/overall", templateUrl: "templates/overall.html" })
+                .state("inventory", { parent:"dashboard", url: "/inventory", templateUrl: "templates/inventory.html" })
+                .state("injuries", { parent: "dashboard", url: "/injuries", templateUrl: "templates/injuries.html" })
+                .state("costs", { parent:"dashboard", url: "/costs", templateUrl: "templates/costs.html" })
+            .state("admin", { url: "/admin", templateUrl: "templates/admin.html" });
     });
-
 }());
