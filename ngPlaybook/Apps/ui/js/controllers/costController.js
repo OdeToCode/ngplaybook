b9e48eaa9b2098b6aaff859289eec81ad5cb5697
K. Scott Allen <scott@OdeToCode.com>
2014-12-20 20:18:51 -0500

    charts complete

---
+++ ngPlaybook/Apps/ui/js/controllers/costController.js
@@ -0,0 +1,47 @@
+﻿
+(function (module) {
+
+    var createChart = function () {
+        var chart = {};
+        chart.type = "PieChart";
+
+        chart.data = [
+            ["Type", "Amount"],
+            ["Food", 34],
+            ["Salaries", 12],
+            ["Lawsuits", 20],
+            ["Drinks", 23],
+            ["Silverware", 12]
+        ];
+
+        chart.options = {
+            height: 300,
+            width: 600,
+            backgroundColor: "#222222",
+            legend: {textStyle: {color: "white", fontSize: 16}},
+            animation: {
+                duration: 500,
+                easing: "out"
+            }
+        };
+        return chart;
+    };
+
+    var addData = function (chart) {
+        for (var i = 1; i < chart.data.length; i++) {
+            chart.data[i][1] = Math.round(Math.random() * 50) + 5;
+        }
+    };
+
+    var costController = function ($timeout) {
+        var self = this;
+        self.chart = createChart();
+        $timeout(function () {
+            self.showChart = true;
+            addData(self.chart);
+        }, 750);
+    };
+
+    module.controller("costController", costController);
+
+}(angular.module("ui")));
---
+++ ngPlaybook/Apps/ui/js/controllers/injuriesController.js
@@ -0,0 +1,53 @@
+﻿
+(function (module) {
+
+    var createChart = function () {
+        var chart = {};
+        chart.type = "ColumnChart";
+
+        chart.data = [
+            ["Injuries", "Count", {role:"style"}],
+            ["Major Burns", 0, ""],
+            ["Toxic Inhalations", 0, ""],
+            ["Lacerations", 0, ""],
+            ["Discoball Contusions", 0, ""],
+            ["Muscle Pulls", 0, ""],
+            ["Food poisonings", 0, ""],
+            ["Hallucinations", 0, ""]
+        ];
+
+        chart.options = {
+            hAxis: { textStyle: { color: "#ffffff"}},
+            vAxis: { title: "Injury Type", textStyle:{color:"#ffffff"} },
+            height: 400,
+            backgroundColor: "#222222",
+            legend: { textStyle: { color: "white", fontSize: 16 } },
+            animation: {
+                duration: 500,
+                easing: "out"
+            }
+        };
+        return chart;
+    };
+
+    var addData = function (chart) {
+        for (var i = 1; i < chart.data.length; i++) {
+            chart.data[i][1] = Math.round(Math.random() * 50) + 5;
+            if (chart.data[i][1] > 40) {
+                chart.data[i][2] = "red";
+            }
+        }
+    };
+
+    var injuriesController = function ($timeout) {
+        var self = this;
+        self.chart = createChart();
+        $timeout(function () {
+            self.showChart = true;
+            addData(self.chart);
+        }, 750);
+    };
+
+    module.controller("injuriesController", injuriesController);
+
+}(angular.module("ui")));
